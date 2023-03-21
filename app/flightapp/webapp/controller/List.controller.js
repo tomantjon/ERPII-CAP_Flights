74cf28510dfd563d0856193f389851e14ad67895
Tom Antjon <tom.antjon@gmail.com>
2023-03-21 12:53:02 +0100

    Moved App content to new List view + routing

---
+++ app/flightapp/webapp/controller/List.controller.js
@@ -0,0 +1,40 @@
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, JSONModel, MessageBox) {
+    "use strict";
+
+    return Controller.extend("hogent.flightapp.controller.List", {
+      onInit: function () {
+        var oFlight = {
+          carrID: "",
+          connID: "",
+          fldate: null,
+        };
+        var oModel = new JSONModel(oFlight);
+        this.getView().setModel(oModel, "form");
+      },
+      handleSavePress: function () {
+        var oForm = this.getView().getModel("form").getData();
+        oForm.fldate = new Date(oForm.fldate);
+
+        var odatamodel = this.getView().getModel("v2model");
+
+        odatamodel.create("/Flights", oForm, {
+          success: function (data, response) {
+            MessageBox.success("Data was created successfully");
+          },
+          error: function (error) {
+            MessageBox.error("Error while creating the data");
+          },
+        });
+      },
+    });
+  }
+);
