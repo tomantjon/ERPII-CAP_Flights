b91d5dbb29ec1a7c4eab68ce210220d126c99660
Tom Antjon <tom.antjon@gmail.com>
2023-03-27 22:40:34 +0200

    Updated detail page to fetch the data

--- app/flightapp/webapp/controller/Detail.controller.js
+++ app/flightapp/webapp/controller/Detail.controller.js
@@ -13,8 +13,43 @@ sap.ui.define(
         this.oRouter.attachRouteMatched(this._onRouteMatched, this);
       },
       _onRouteMatched: function (oEvent) {
-        var oArgs;
-        oArgs = oEvent.getParameter("arguments");
+        var oArgs = oEvent.getParameter("arguments");
+        var oView = this.getView();
+        var oDataModel = oView.getModel("v2model");
+
+        //oArgs.fldate = new Date(oArgs.fldate).toJSON();
+        var urlPath =
+          "/Flights(carrID='" +
+          oArgs.carrid +
+          "',connID='" +
+          oArgs.connid +
+          "',fldate=datetimeoffset'" +
+          encodeURI(oArgs.fldate.substr(0, 20)) +
+          "')";
+
+        oView.bindElement({ path: urlPath, model: "v2model" });
+
+        this.readElement(urlPath, oDataModel).done(
+          function (oData) {
+            oDataModel.refresh(true);
+            var newModel = this.getView().getModel("v2model");
+          }.bind(this)
+        );
+      },
+
+      readElement: function (path, odatamodel, filter) {
+        var oDeferred = jQuery.Deferred();
+
+        odatamodel.read(path, {
+          filters: [filter],
+          success: function (oData) {
+            return oDeferred.resolve(oData);
+          }.bind(this),
+          error: function (oError) {
+            return oDeferred.reject(oError);
+          }.bind(this),
+        });
+        return oDeferred.promise();
       },
     });
   }
