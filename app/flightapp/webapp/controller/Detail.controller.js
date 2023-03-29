533e62e56db7c0c706255a519d8f602868d73096
Tom Antjon <tom.antjon@gmail.com>
2023-03-29 16:30:07 +0200

    Added new gameservice and valuehelper dialog

--- app/flightapp/webapp/controller/Detail.controller.js
+++ app/flightapp/webapp/controller/Detail.controller.js
@@ -1,9 +1,14 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller"],
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/Token",
+    "sap/ui/table/Column",
+    "sap/m/Column",
+  ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller) {
+  function (Controller, Token, UIColumn, MColumn) {
     "use strict";
 
     return Controller.extend("hogent.flightapp.controller.Detail", {
@@ -32,9 +37,132 @@ sap.ui.define(
         this.readElement(urlPath, oDataModel).done(
           function (oData) {
             oDataModel.refresh(true);
-            var newModel = this.getView().getModel("v2model");
+
+            this._setDefaultTokens(oData);
           }.bind(this)
         );
+      },
+
+      _setDefaultTokens: function (oData) {
+        var oMultiInput = this.byId("multiInput");
+        //oMultiInput.addValidator(this._onMultiInputValidate);
+        oMultiInput.setTokens(this._getDefaultTokens());
+        this._oMultiInput = oMultiInput;
+      },
+
+      // #region Value Help Dialog standard use case with filter bar without filter suggestions
+      onValueHelpRequested: function () {
+        if (!this.pDialog) {
+          this.pDialog = this.loadFragment({
+            name: "hogent.flightapp.fragment.ValueHelpDialogGame",
+          });
+        }
+        this.pDialog.then(
+          function (oDialog) {
+            this._oVHD = oDialog;
+            // Initialise the dialog with model only the first time. Then only open it
+            if (this._bDialogInitialized) {
+              // Re-set the tokens from the input and update the table
+              oDialog.setTokens([]);
+              oDialog.setTokens(this._oMultiInput.getTokens());
+              oDialog.update();
+
+              oDialog.open();
+              return;
+            }
+            this.getView().addDependent(oDialog);
+
+            oDialog.getTableAsync().then(
+              function (oTable) {
+                //get the games ODataModel
+                var oDataModel = this.getView().getModel("v2gameModel");
+                oTable.setModel(oDataModel);
+
+                // For Desktop and tabled the default table is sap.ui.table.Table
+                if (oTable.bindRows) {
+                  // Bind rows to the ODataModel and add columns
+                  oTable.bindAggregation("rows", {
+                    path: "/Games",
+                    events: {
+                      dataReceived: function () {
+                        oDialog.update();
+                      },
+                    },
+                  });
+                  oTable.addColumn(
+                    new UIColumn({
+                      label: "Game",
+                      template: "Name",
+                    })
+                  );
+                  oTable.addColumn(
+                    new UIColumn({
+                      label: "Publisher",
+                      template: "Developer",
+                    })
+                  );
+                }
+
+                // For Mobile the default table is sap.m.Table
+                if (oTable.bindItems) {
+                  // Bind items to the ODataModel and add columns
+                  oTable.bindAggregation("items", {
+                    path: "/Games",
+                    template: new ColumnListItem({
+                      cells: [
+                        new Label({ text: "{Name}" }),
+                        new Label({ text: "{Developer}" }),
+                      ],
+                    }),
+                    events: {
+                      dataReceived: function () {
+                        oDialog.update();
+                      },
+                    },
+                  });
+                  oTable.addColumn(
+                    new MColumn({ header: new Label({ text: "Game" }) })
+                  );
+                  oTable.addColumn(
+                    new MColumn({ header: new Label({ text: "Publisher" }) })
+                  );
+                }
+                oDialog.update();
+              }.bind(this)
+            );
+
+            oDialog.setTokens(this._oMultiInput.getTokens());
+
+            // set flag that the dialog is initialized
+            this._bDialogInitialized = true;
+            oDialog.open();
+          }.bind(this)
+        );
+      },
+
+      onValueHelpOkPress: function (oEvent) {
+        var aTokens = oEvent.getParameter("tokens");
+        this._oMultiInput.setTokens(aTokens);
+        this._oVHD.close();
+      },
+
+      onValueHelpCancelPress: function () {
+        this._oVHD.close();
+      },
+
+      // Internal helper methods
+      _getDefaultTokens: function () {
+        var oToken1 = new Token({
+          key: "PD-103",
+          text: "Mario Bros",
+        });
+
+        var oToken2 = new Token({
+          key: "range_0",
+          text: "Pac Man",
+        });
+
+        return [oToken1, oToken2];
       },
 
       readElement: function (path, odatamodel, filter) {
