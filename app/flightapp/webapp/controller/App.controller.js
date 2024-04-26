73a494231f1ea273eaa0bdfb63fa92e68e51f309
Tom Antjon <tomtjen@gmail.com>
2024-04-26 14:36:38 +0200

    Added SimpleForm and handlePress

--- app/flightapp/webapp/controller/App.controller.js
+++ app/flightapp/webapp/controller/App.controller.js
@@ -1,15 +1,16 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageBox"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageBox) {
+    "use strict";
 
-        return Controller.extend("hogent.flightapp.controller.App", {
-            onInit: function () {
-
-            }
-        });
+    return Controller.extend("hogent.flightapp.controller.App", {
+      onInit: function () {},
+      handleSavePress: function () {
+        MessageBox.success("Add Button clicked");
+      },
     });
+  }
+);
