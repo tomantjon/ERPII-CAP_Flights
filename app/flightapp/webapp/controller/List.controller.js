a954922953a992495e34a95a85950c5b6c9d435d
Tom Antjon <tomtjen@gmail.com>
2024-04-26 15:31:13 +0200

    - Added List view and controller - Moved App content to List

---
+++ app/flightapp/webapp/controller/List.controller.js
@@ -0,0 +1,52 @@
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageBox, JSONModel) {
+    "use strict";
+
+    return Controller.extend("hogent.flightapp.controller.List", {
+      onInit: function () {
+        var oFlight = {
+          carrID: "",
+          connID: "",
+          fldate: null,
+        };
+        var oModel = new JSONModel(oFlight);
+        this.getView().setModel(oModel, "form");
+      },
+      handleSavePress: function () {
+        var oForm = this.getView().getModel("form").getData();
+        oForm.fldate = new Date(oForm.fldate);
+
+        this._createFlightV4(oForm);
+      },
+
+      _createFlightV4: function (oFlight) {
+        var oContext = this.getView()
+          .byId("idFlightsTable")
+          .getBinding("items")
+          .create(oFlight);
+
+        oContext.created().then(
+          function () {
+            // Flight successfully created
+            MessageBox.success("Data was created successfully");
+          },
+          function (oError) {
+            MessageBox.error("Error while creating the data");
+            // handle rejection of entity creation; if oError.canceled === true then the transient entity has been deleted
+            if (!oError.canceled) {
+              throw oError; // unexpected error
+            }
+          }
+        );
+      },
+    });
+  }
+);
