d24847091bf00fc16669d826f22696e091d32f55
Tom Antjon <tomtjen@gmail.com>
2024-04-26 14:38:49 +0200

    Added JSONModel to store form data

--- app/flightapp/webapp/controller/App.controller.js
+++ app/flightapp/webapp/controller/App.controller.js
@@ -1,14 +1,28 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageBox"],
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel",
+  ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, MessageBox) {
+  function (Controller, MessageBox, JSONModel) {
     "use strict";
 
     return Controller.extend("hogent.flightapp.controller.App", {
-      onInit: function () {},
+      onInit: function () {
+        var oFlight = {
+          carrID: "",
+          connID: "",
+          fldate: null,
+        };
+        var oModel = new JSONModel(oFlight);
+        this.getView().setModel(oModel, "form");
+      },
       handleSavePress: function () {
+        var oForm = this.getView().getModel("form").getData();
+        console.log(oForm);
         MessageBox.success("Add Button clicked");
       },
     });
