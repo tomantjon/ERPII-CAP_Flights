1bb5271952e8a7408b6e97d7f0dee93633123fbd
Tom Antjon <tomtjen@gmail.com>
2024-05-03 11:32:08 +0200

    - Added detail view and controller - Added handlePress to show the detail view in 2 column mode

--- app/flightfcl/webapp/controller/List.controller.js
+++ app/flightfcl/webapp/controller/List.controller.js
@@ -1,13 +1,17 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller"],
+  ["sap/ui/core/mvc/Controller", "sap/f/library"],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller) {
+  function (Controller, fioriLibrary) {
     "use strict";
 
     return Controller.extend("hogent.flightfcl.controller.List", {
       onInit: function () {},
+      handleListItemPress: function (oEvent) {
+        var oFCL = this.getView().getParent().getParent();
+        oFCL.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
+      },
     });
   }
 );
