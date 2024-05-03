1577ce7fa70b4aac1e20450b1244845e1c0f2bbb
Tom Antjon <tomtjen@gmail.com>
2024-05-03 13:24:33 +0200

    - Passed flightpath on navigation - Loaded the flight detail in detail panel

--- app/flightfcl/webapp/controller/Detail.controller.js
+++ app/flightfcl/webapp/controller/Detail.controller.js
@@ -7,7 +7,36 @@ sap.ui.define(
     "use strict";
 
     return Controller.extend("hogent.flightfcl.controller.Detail", {
-      onInit: function () {},
+      onInit: function () {
+        var oOwnerComponent = this.getOwnerComponent();
+
+        this.oRouter = oOwnerComponent.getRouter();
+        this.oModel = oOwnerComponent.getModel();
+
+        this.oRouter
+          .getRoute("List")
+          .attachPatternMatched(this._onRouteMatched, this);
+        this.oRouter
+          .getRoute("Detail")
+          .attachPatternMatched(this._onRouteMatched, this);
+      },
+
+      _onRouteMatched: function (oEvent) {
+        this._product =
+          oEvent.getParameter("arguments").flightpath || this._product || "0";
+        this.getView().bindElement({
+          path: "/" + this._product,
+        });
+      },
+
+      onExit: function () {
+        this.oRouter
+          .getRoute("List")
+          .detachPatternMatched(this._onRouteMatched, this);
+        this.oRouter
+          .getRoute("Detail")
+          .detachPatternMatched(this._onRouteMatched, this);
+      },
     });
   }
 );
